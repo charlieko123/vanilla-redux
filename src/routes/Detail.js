a5336f13b9e7a4c01d6d1786f244c450c38721fd
jongmin <charlieko123@github.com>
2024-10-21 01:24:42 +0900

    React Redux

---
+++ src/routes/Detail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const toDo = useSelector((state) =>
+    state.find((toDo) => toDo.id === parseInt(id))
+  );
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id} </h5>
+    </>
+  );
+}
+
+export default Detail;
